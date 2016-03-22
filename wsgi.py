0f9002f3f498f9cea1441cfc3fb5f605bb3c5f02
Dinesh Kumar <dineshkb@thoughtworks.com>
2016-03-22 23:02:50 +0530

    wsgi using other setup

--- wsgi.py
+++ wsgi.py
@@ -1,21 +1,16 @@
 import sys
 import os
 
-
-sys.path.insert(0, os.path.dirname(__file__) or '.')
-
-PY_DIR = os.path.join(os.environ['OPENSHIFT_HOMEDIR'], "python")
-
-virtenv = PY_DIR + '/virtenv/'
-
-PY_CACHE = os.path.join(virtenv, 'lib', os.environ['OPENSHIFT_PYTHON_VERSION'], 'site-packages')
-
-os.environ['PYTHON_EGG_CACHE'] = os.path.join(PY_CACHE)
+virtenv = os.environ['APPDIR'] + '/virtenv/'
+os.environ['PYTHON_EGG_CACHE'] = os.path.join(virtenv, 'lib/python2.6/site-packages')
 virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
-
 try:
-	    exec(open(virtualenv).read(), dict(__file__=virtualenv))
+    execfile(virtualenv, dict(__file__=virtualenv))
 except IOError:
-	    pass
+    pass
+
 
 from sync_preference_service import application as application
+
+
+
