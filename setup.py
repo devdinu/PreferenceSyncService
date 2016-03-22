5f13f6812f66184bfbf0d1d4ac03d17e04342e62
Dinesh Kumar <dineshkb@thoughtworks.com>
2016-03-22 21:30:56 +0530

    Trying with marker to do clean build

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup
 
-setup(name='PrefSyncService', version='1.0',
+setup(name='PrefSyncService',
+      version='1.0',
       description='PrefSync Service For syncing Sublime Text User Packages',
-      author='Dinesh Kumar', author_email='dineshkumar_cse@hotmail.com',
+      author='Dinesh Kumar',
+      author_email='dineshkumar_cse@hotmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
       install_requires=[ 'bottle', 'pymongo'],
 )
